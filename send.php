5b8d7c2793ebb87cd9b5c06233e91d2b0fe38b7c
Tatiana <tanya_13.88@mail.ru>
2023-03-12 18:32:06 +0300

    first commit

---
+++ send.php
@@ -0,0 +1,27 @@
+  <html>
+  <head>
+  <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+
+      $name = $_POST['name'];
+
+      $mail = $_POST['mail'];
+
+      $tel = $_POST['tel'];
+
+
+
+      print "Ваше имя - $name
+      <br>
+      почта - $mail
+       <br>
+       телефон - $tel
+       <br>";
+
+
+     ?>
+
+   </body>
+  </html>
